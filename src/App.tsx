224bd241921a409b322c07920a2c525dfc5b4ae9
ngocbuidhv <ngoc2001bui@gmail.com>
2023-10-04 19:59:49 +0700

    init header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/header-footer/Navbar';
 
 function App() {
   return (
-    <div className="App">
-        BOOKSTORE PROJECT
+    <div className='App'>
+      <Navbar />
     </div>
   );
 }
